e6d8f1555e7c344fea50cdf4142d825627b412b2
Shakir-After85 <shakir.tamar@gmail.com>
2022-10-02 03:52:55 +0000

    Simple Counter

---
+++ src/js/component/clockdigits.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ClockDigits = (props) => {
+    return(
+        <div className="clock-indices">
+            <h1>{props.value}</h1>
+        </div>
+    )
+}
+
+export default ClockDigits; 
